cb36454ac5e896c38c8cd76e4661c99dd7827772
guillcodes <guilcodez@gmail.com>
2025-03-20 20:00:41 -0300

    arquivo atulizado com o desafio concluido

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args){
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o número de sua Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Digite o número de sua Conta: ");
+        int numero = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.print("Digite seu Nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Informe o saldo da sua conta: ");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nome + ", obrigado por criar uma conta em nosso banco, " + 
+                "sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + 
+                " já está disponível para efetuar saque saque.");
+
+        scanner.close();
     }
 }
